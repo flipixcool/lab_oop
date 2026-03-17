f990b3c786717e9fdf60524ca9cc0b1842648a3c
flipixcool <danilasavcenko5@gmail.com>
2026-03-17 12:24:27 +0300

    make new file validate.py and moved the code to this file

--- src/model.py
+++ src/model.py
@@ -1,29 +1,9 @@
 from cryptography.fernet import Fernet
 from uuid import uuid4
-from typing import List, Dict, Any
+from typing import List, Dict
 import re
 from datetime import datetime
-
-
-class Validators:
-    @staticmethod
-    def validate_not_empty(value: Any, field_name: str):
-        if value is None or (isinstance(value, str) and not value.strip()) or (isinstance(value, list) and len(value) == 0):
-            raise ValueError(f"{field_name} не может быть пустым")
-
-    @staticmethod
-    def validate_positive(value: float | int | None, field_name: str):
-        if value is None:
-            raise ValueError(f"{field_name} не может быть пустым")
-        if value < 0:
-            raise ValueError(f"{field_name} не может быть меньше 0")
-
-    @staticmethod
-    def validate_range(value: int | None, field_name: str, min_val: int, max_val: int):
-        if value is None:
-            raise ValueError(f"{field_name} не может быть пустым")
-        if not min_val <= value <= max_val:
-            raise ValueError(f"{field_name} должен быть от {min_val} до {max_val}")
+from validate import Validators
 
 
 class Order:
